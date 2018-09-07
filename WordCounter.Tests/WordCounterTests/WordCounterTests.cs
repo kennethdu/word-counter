2fa0e5799b5100c11c438c116be86a35418b33df
Kenneth Du <kennethdu3@gmail.com>
2018-09-07 11:46:27 -0700

    pass second test identifying a user sentence

--- WordCounter.Tests/WordCounterTests/WordCounterTests.cs
+++ WordCounter.Tests/WordCounterTests/WordCounterTests.cs
@@ -12,19 +12,31 @@ namespace WordCounter.Tests
     public void GetWord_ReturnUserWord_String()
     {
       //Arrange
-      string word = "hello";
-      RepeatCounter newWord = new RepeatCounter(word);
+      RepeatCounter userInput = new RepeatCounter("dog", "my dog is the best dog");
       //Act
-      string result = newWord.GetWord();
+      string word = userInput.GetWord();
       //Assert
-      Assert.AreEqual("hello", result);
+      Assert.AreEqual("dog", word);
     }
     [TestMethod]
-    publid void SplitSentence_SplitUserSentence_String()
+    public void GetSentence_ReturnUserSentence_String()
     {
       //Arrange
+      RepeatCounter userInput = new RepeatCounter("dog", "my dog is the best dog");
       //Act
+      string sentence = userInput.GetSentence();
       //Assert
+      Assert.AreEqual("my dog is the best dog", sentence);
+    }
+    [TestMethod]
+    public void SplitSentence_SplitUserSentence_String()
+    {
+      //Arrange
+      RepeatCounter userInput = new RepeatCounter("dog", "my dog is the best dog");
+      //Act
+      string[] sentence = userInput.SplitSentence();
+      //Assert
+      Assert.AreEqual("my", splitSentence[0]);
     }
     // [TestMethod]
     // publid void Compare_CheckIfWordAppearsInSentence_bool()
--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -6,17 +6,33 @@ namespace WordCounter.Models
   public class RepeatCounter
   {
     private string _word;
-    public RepeatCounter (string word)
+    private string _sentence;
+    public RepeatCounter (string word, string sentence)
     {
       _word = word;
+      _sentence = sentence;
     }
-    public void SetWord(string input)
+    public void SetWord(string inputWord)
     {
-      _word = input;
+      _word = inputWord;
     }
     public string GetWord()
     {
       return _word;
     }
+    public void SetSentence(string sentenceInput)
+    {
+      _sentence = sentenceInput;
+    }
+    public string GetSentence()
+    {
+      return _sentence;
+    }
+    public string[] SplitSentence()
+    {
+      string[] newArray = _sentence.Split(' ');
+      return newArray;
+    }
+
   }
 }
