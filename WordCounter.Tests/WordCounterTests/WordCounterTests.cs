dcfd9647fbfcb129c5b27666c39a5fa5fda14776
Kenneth Du <kennethdu3@gmail.com>
2018-09-07 09:46:25 -0700

    start first test to return user word

--- WordCounter.Tests/WordCounterTests/WordCounterTests.cs
+++ WordCounter.Tests/WordCounterTests/WordCounterTests.cs
@@ -1,45 +1,51 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using ScrabbleScore.Models;
+using WordCounter.Models;
 using System;
 
 namespace WordCounter.Tests
 {
   [TestClass]
-  public class RepeatCounter()
+  public class Word()
   {
     [TestMethod]
     publid void GetWord_ReturnUserWord_String()
     {
       //Arrange
+      string word = "hello";
+      Word newWord = new Word(word);
       //Act
+      string result = newWord.GetWord();
       //Assert
+      Assert.AreEqual("hello", result);
     }
-    [TestMethod]
-    publid void GetSentence_ReturnUserSentence_String()
-    {
-      //Arrange
-      //Act
-      //Assert
-    }
-    [TestMethod]
-    publid void Compare_CheckIfWordAppearsInSentence_bool()
-    {
-      //Arrange
-      //Act
-      //Assert
-    }
-    [TestMethod]
-    publid void Count_Add1toWordCount_Int()
-    {
-      //Arrange
-      //Act
-      //Assert
-    }
-
-
-
-
+    // [TestMethod]
+    // publid void GetSentence_ReturnUserSentence_String()
+    // {
+    //   //Arrange
+    //   //Act
+    //   //Assert
+    // }
+    // [TestMethod]
+    // publid void SplitSentence_SplitUserSentence_String()
+    // {
+    //   //Arrange
+    //   //Act
+    //   //Assert
+    // }
+    // [TestMethod]
+    // publid void Compare_CheckIfWordAppearsInSentence_bool()
+    // {
+    //   //Arrange
+    //   //Act
+    //   //Assert
+    // }
+    // [TestMethod]
+    // publid void Count_Add1toWordCount_Int()
+    // {
+    //   //Arrange
+    //   //Act
+    //   //Assert
+    // }
   }
-
 }
--- WordCounter/WordCounter/WordCounter.cs
+++ WordCounter/WordCounter/WordCounter.cs
@@ -3,8 +3,22 @@ using System.Collections.Generic;
 
 namespace WordCounter
 {
-  public class RepeatCounter ()
+  public class Word
   {
+    private string _word;
+
+    public Word (string word)
+    {
+      _word = word;
+    }
+    public void SetWord(string input)
+    {
+      _word = input;
+    }
+    public string GetWord()
+    {
+      return _word;
+    }
 
   }
 }
