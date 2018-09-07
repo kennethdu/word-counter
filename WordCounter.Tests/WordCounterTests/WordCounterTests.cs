b9de0aebd3b8531b08a37b5df6896b7398c1b08e
Kenneth Du <kennethdu3@gmail.com>
2018-09-07 09:15:07 -0700

    add tests to to code.

---
+++ WordCounter.Tests/WordCounterTests/WordCounterTests.cs
@@ -0,0 +1,45 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ScrabbleScore.Models;
+using System;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class RepeatCounter()
+  {
+    [TestMethod]
+    publid void GetWord_ReturnUserWord_String()
+    {
+      //Arrange
+      //Act
+      //Assert
+    }
+    [TestMethod]
+    publid void GetSentence_ReturnUserSentence_String()
+    {
+      //Arrange
+      //Act
+      //Assert
+    }
+    [TestMethod]
+    publid void Compare_CheckIfWordAppearsInSentence_bool()
+    {
+      //Arrange
+      //Act
+      //Assert
+    }
+    [TestMethod]
+    publid void Count_Add1toWordCount_Int()
+    {
+      //Arrange
+      //Act
+      //Assert
+    }
+
+
+
+
+  }
+
+}
